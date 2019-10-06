413894223300b522d091dc0394bf8fe1ffba5f16
Erwin Heba <e.heba@getprintbox.com>
2019-10-06 22:10:18 +0200

    prod to gh pages

---
+++ build/precache-manifest.0d707b2496823d428fe3e4ff4c3c1e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5732f3a4676494344c60b7883bc1c62f",
+    "url": "/PokeApp_rek/index.html"
+  },
+  {
+    "revision": "8603586dd162d6e12915",
+    "url": "/PokeApp_rek/static/js/2.bf4c2ae1.chunk.js"
+  },
+  {
+    "revision": "43f51095e8a7309c7858",
+    "url": "/PokeApp_rek/static/js/main.2aa5973d.chunk.js"
+  },
+  {
+    "revision": "159e85fc2ce9b31f5bc5",
+    "url": "/PokeApp_rek/static/js/runtime-main.a42b73e7.js"
+  },
+  {
+    "revision": "1d15b24dac9586491e5c978d94861d73",
+    "url": "/PokeApp_rek/static/media/pokeLogo.1d15b24d.png"
+  }
+]);
